fe47b7b192364c48f35a14183dcd44ab5288a8ed
HP <sstico0@gmail.com>
2021-09-21 22:21:18 +0530

    icko and ticko added

---
+++ FScelebClash.bat
@@ -0,0 +1,7 @@
+set idir="D:\Developed\FaceSwapExperimental\TestResult" 
+set chdir="D:\paradise\stuff\Images\CFSC\champs" 
+set middir="D:\paradise\stuff\Images\CFSC\1" 
+set deldir="C:\temp\deleatble" 
+
+
+python "D:\Developed\Automation\imageClasher\icko.py"  --inputDir %idir% --championsDir %chdir% --MidCardDir %middir% --DeletablePath %deldir%  --time 5 --winRate 5
---
+++ tickoset.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import sys
+import pandas as pd
+from random import shuffle
+import random
+import numpy as np
+import os
+import re
+import shutil
+from tqdm import tqdm
+import argparse
+
+ipath = r'C:\Heaven\YummyBaked'
+clashFilep = r'D:\Developed\Automation\imageClasher\ticko.py'
+cpd = r'C:\Heaven\YummyBaked\champions'
+mpd = r'C:\Heaven\YummyBaked\midCard'
+# breakpoint()
+sp = [dp for dp in Path(ipath).glob('*/')]
+random.shuffle(sp)
+dp = sp[0]
+cmd = 'python "%s" --inputDir "%s" ' % (clashFilep, str(dp))
+os.system(cmd)
+# for dp in Path(ipath).glob('*/'):
