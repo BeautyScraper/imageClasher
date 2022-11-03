552c6801b6e10b29aab3dc261a0f5db414cbba7b
BeautyScraper <sstico0@gmail.com>
2022-11-03 12:54:26 +0530

    move level incorporated

--- csvheap_ops.py
+++ csvheap_ops.py
@@ -15,7 +15,35 @@ def csv_to_dir():
         out_dir_level = Path(out_dir) / level / file_name
         out_dir_level.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy(file_path, out_dir_level)
+        
+def dir_to_csv():
+    df = pd.read_csv(csfv_file_path)
+    lev_dirs = [x for x in Path(out_dir).iterdir() if x.is_dir()] 
+    lev_dirs_name = [x.name for x in Path(out_dir).iterdir() if x.is_dir()] 
+    for i,dirs in enumerate(lev_dirs):
+        if str(i) in lev_dirs_name:
+            continue
+        shifted_name = min([x for x in lev_dirs if int(x.name) in range(i+1,max([int(x) for x in lev_dirs_name]))])
+        shifted_name = str(shifted_name)
+        dirs.rename(shifted_name)
 
+def move_file(filePath,out_dir):
+    shutil.move(filePath,out_dir)
+
+def move_level(level, out_dir, csv_file):
+    csv_file = csfv_file_path
+    df = pd.read_csv(csv_file)
+    start_index = 2**level - 1
+    end_index = 2**(level+1) - 2
+    nf = lambda x:move_file(x, out_dir)
+    df.iloc[start_index:end_index,1].apply(nf)
+    df.drop(df.iloc[start_index:end_index,:])
+    Path(csv_file).unlink()
+    df.to_csv(csv_file,index=False)
+
+
+
+ 
 if __name__ == '__main__':
     csv_to_dir()
     
